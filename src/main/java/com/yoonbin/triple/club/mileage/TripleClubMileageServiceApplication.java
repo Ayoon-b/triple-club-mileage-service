3dea4129fbb75d443ca4f96521c931a0901f5270
Ayoon-b <wnr0303@gmail.com>
2022-06-29 22:40:01 +0900

    init

---
+++ src/main/java/com/yoonbin/triple/club/mileage/TripleClubMileageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.yoonbin.triple.club.mileage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripleClubMileageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TripleClubMileageServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yoonbin/triple/club/mileage/TripleClubMileageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yoonbin.triple.club.mileage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripleClubMileageServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
