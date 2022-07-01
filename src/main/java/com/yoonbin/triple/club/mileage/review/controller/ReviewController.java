758c2da7afc422e6b186f94e69bbd924bf3cbb56
Ayoon-b <wnr0303@gmail.com>
2022-07-02 03:12:56 +0900

    chore: Review 생성, 추가, 삭제 ReviewController 구현

--- src/main/java/com/yoonbin/triple/club/mileage/review/controller/ReviewController.java
+++ src/main/java/com/yoonbin/triple/club/mileage/review/controller/ReviewController.java
@@ -1,21 +1,28 @@
 package com.yoonbin.triple.club.mileage.review.controller;
 
 import com.yoonbin.triple.club.mileage.review.domain.Review;
-import com.yoonbin.triple.club.mileage.review.domain.ReviewDto;
 import com.yoonbin.triple.club.mileage.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 public class ReviewController {
     private final ReviewService reviewService;
 
-    @PutMapping("/events")
-    public ReviewDto saveReview(@RequestBody Review review){
+    @PutMapping("/review")
+    public Review saveReview(@RequestBody Review review){
         return reviewService.insertReview(review);
+    }
+
+    @PutMapping("/review/{id}")
+    public Review updateReview(@RequestParam String id, @RequestBody Review review) {
+        return reviewService.updateReview(id, review);
+    }
+
+    @DeleteMapping("/review/{id}")
+    public void deleteReview(@RequestParam String id) {
+        reviewService.deleteReview(id);
     }
 }
 
