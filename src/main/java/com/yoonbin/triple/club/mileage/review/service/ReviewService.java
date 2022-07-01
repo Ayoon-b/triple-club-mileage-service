f471e4e5d85813d5ab64678e0cd20e25072965ff
Ayoon-b <wnr0303@gmail.com>
2022-07-02 03:11:34 +0900

    fix: 리뷰 포인트 부여 기능 ReviewService 구현

--- src/main/java/com/yoonbin/triple/club/mileage/review/service/ReviewService.java
+++ src/main/java/com/yoonbin/triple/club/mileage/review/service/ReviewService.java
@@ -3,7 +3,6 @@ package com.yoonbin.triple.club.mileage.review.service;
 import com.yoonbin.triple.club.mileage.point.domain.Point;
 import com.yoonbin.triple.club.mileage.point.repository.PointRepository;
 import com.yoonbin.triple.club.mileage.review.domain.Review;
-import com.yoonbin.triple.club.mileage.review.domain.ReviewDto;
 import com.yoonbin.triple.club.mileage.review.repository.ReviewRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -22,6 +21,10 @@ public class ReviewService {
 
     public List<Review> getReviews(){
         return reviewRepository.findAll();
+    }
+
+    public Review saveReview(Review review){
+        return reviewRepository.save(review);
     }
 
     public List<Review> getReviewsByPlaceId(String placeId){
@@ -44,7 +47,7 @@ public class ReviewService {
     public boolean checkPhoto(Review review){
         boolean checkPhoto = false;
 
-        if(!review.getAttachedPhotoIds().get(0).isEmpty()){
+        if(!review.getAttachedPhotoIds().isEmpty() && !review.getAttachedPhotoIds().get(0).isEmpty()){
             checkPhoto = true;
         }
         return checkPhoto;
@@ -58,6 +61,38 @@ public class ReviewService {
         }
 
         return checkPlace;
+    }
+    public Map.Entry<Integer, String> updateAmount(Review review){
+        int amount = 0;
+        String remarks ="";
+        Review preReview = reviewRepository.findById(review.getReviewId()).get();
+
+
+        if(review.getContent().isEmpty()){
+            if(!preReview.getContent().isEmpty()){
+                amount--;
+                remarks+="텍스트 삭제";
+            }
+        } else {
+            if(preReview.getContent().isEmpty()){
+                amount++;
+                remarks+="텍스트 추가";
+            }
+        }
+
+        if(review.getAttachedPhotoIds().isEmpty() || review.getAttachedPhotoIds().get(0).isEmpty()){
+            if(!preReview.getContent().isEmpty()){
+                amount--;
+                remarks+="사진 삭제";
+            }
+        } else if (!review.getAttachedPhotoIds().isEmpty() && !review.getAttachedPhotoIds().get(0).isEmpty()){
+            if(preReview.getAttachedPhotoIds().isEmpty() || preReview.getAttachedPhotoIds().get(0).isEmpty()) {
+                amount++;
+                remarks+="사진 추가";
+            }
+        }
+        AbstractMap.Entry<Integer, String> updateAmount =new AbstractMap.SimpleEntry<>(amount , remarks);
+        return updateAmount;
     }
 
     public Map.Entry<Integer, String> checkAmount(Review review){
@@ -108,12 +143,11 @@ public class ReviewService {
     }
 
     @Transactional
-    public ReviewDto insertReview(Review review){
+    public Review insertReview(Review review){
         AbstractMap.Entry<Integer, String> checkAmount = checkAmount(review);
-        ReviewDto reviewDto = null;
         if(checkAmount.getKey() > 0) {
             if (!visitedCheck(review)) {
-                Review saveReview = reviewRepository.save(review);
+                reviewRepository.save(review);
 
                 Point point = Point.builder()
                         .reviewId(review.getReviewId())
@@ -124,20 +158,73 @@ public class ReviewService {
                         .build();
                 pointRepository.save(point);
 
-                reviewDto = ReviewDto.builder()
-                        .type("REVIEW")
-                        .action("ADD")
-                        .reviewId(saveReview.getReviewId())
-                        .content(saveReview.getContent())
-                        .attachedPhotoIds(saveReview.getAttachedPhotoIds())
-                        .placeId(saveReview.getPlaceId())
-                        .userId(saveReview.getUserId())
-                        .build();
-
             } else System.out.println("해당 장소에 이미 작성된 리뷰가 존재합니다.");
         }else System.out.println("텍스트와 사진이 작성되지 않아 리뷰로 등록되지 않습니다.");
 
-        return reviewDto;
+        return review;
+    }
+
+    @Transactional
+    public Review updateReview(String reviewId, Review review){
+        AbstractMap.Entry<Integer, String> checkAmount = checkAmount(review);
+        Review updateReview = reviewRepository.findById(reviewId).
+                orElseThrow(() -> new
+                IllegalArgumentException("해당 리뷰가 존재하지 않습니다."));
+
+        if(review.getContent().isEmpty() && review.getAttachedPhotoIds().isEmpty()){
+            System.out.println("내용이 비어있습니다.");
+            return review;
+        }
+
+        if(!review.getContent().isEmpty()){
+            updateReview.setContent(review.getContent());
+        }
+        if(!review.getAttachedPhotoIds().isEmpty()){
+            updateReview.setAttachedPhotoIds(review.getAttachedPhotoIds());
+        }
+
+        Point point = Point.builder()
+                .reviewId(updateReview.getReviewId())
+                .remarks(checkAmount.getValue())
+                .amount(checkAmount.getKey())
+                .placeId(updateReview.getPlaceId())
+                .userId(updateReview.getUserId())
+                .build();
+        pointRepository.save(point);
+
+        return review;
+    }
+
+    @Transactional
+    public void deleteReview(String reviewId) {
+        try {
+            Review review = reviewRepository.findById(reviewId).get();
+            int amount = 0;
+            for (Point p : pointRepository.findByPlaceId(review.getPlaceId())) {
+                if(p.getUserId().equals(review.getUserId())){
+                    amount+=p.getAmount();
+                }
+            }
+            if(amount>0) amount = -(amount);
+
+            Point point = Point.builder()
+                    .reviewId(review.getReviewId())
+                    .remarks("리뷰 삭제")
+                    .amount(amount)
+                    .placeId(review.getPlaceId())
+                    .userId(review.getUserId())
+                    .build();
+            pointRepository.save(point);
+
+            reviewRepository.delete(review);
+
+        } catch (NullPointerException e) {
+            throw new NullPointerException(e.getMessage());
+        }
+    }
+
+    public Review getReviewByReviewId(String reviewId) {
+        return reviewRepository.findById(reviewId).get();
     }
 }
 
