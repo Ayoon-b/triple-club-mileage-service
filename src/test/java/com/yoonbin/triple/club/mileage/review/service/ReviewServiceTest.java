eaef7ecdd8909537758ae2469c85e4ca9850dc13
Ayoon-b <wnr0303@gmail.com>
2022-07-01 19:00:53 +0900

    test: ReviewService 메서드 변경에 따른 테스트

--- src/test/java/com/yoonbin/triple/club/mileage/review/service/ReviewServiceTest.java
+++ src/test/java/com/yoonbin/triple/club/mileage/review/service/ReviewServiceTest.java
@@ -6,8 +6,10 @@ import com.yoonbin.triple.club.mileage.review.repository.ReviewRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.LinkedList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,57 +25,103 @@ class ReviewServiceTest {
 
     @Test
     void getReviewsByPlaceIdTest() {
-        List<Review> review = reviewService.getReviewsByPlaceId("49fa8287-6d24-45f5-9525-2b7539bf4eb0");
-        for (Review r : review) {
-            assertThat(r.getPlaceId()).isEqualTo("49fa8287-6d24-45f5-9525-2b7539bf4eb0");
+        List<Review> reviews = new LinkedList<>();
+        Review review = reviewRepository.save(Review.builder()
+                .content("getReviewsByPlaceIdTest")
+                .attachedPhotoIds(List.of(new String[]{"image1.jpg",
+                        "image2.png", "image3.jpg"}))
+                .placeId("1641ac95-da05-4e76-aecd-8b524d5e408f")
+                .userId("42ab933f-2f37-4131-a784-8e4f889418c9")
+                .build());
+
+        reviews.add(review);
+
+        for (Review r : reviews) {
+            assertThat(r.getPlaceId()).isEqualTo("1641ac95-da05-4e76-aecd-8b524d5e408f");
         }
     }
 
     @Test
     void getReviewsByUserIdTest() {
-        List<Review> review = reviewService.getReviewsByUserId("b0e96d94-1c85-416d-bc35-8b3017dd5a65");
-        for (Review r : review) {
-            assertThat(r.getUserId()).isEqualTo("b0e96d94-1c85-416d-bc35-8b3017dd5a65");
+        List<Review> reviews = new LinkedList<>();
+        Review review = reviewRepository.save(Review.builder()
+                .content("getReviewsByUserIdTest")
+                .attachedPhotoIds(List.of(new String[]{"image1.jpg",
+                        "image2.png", "image3.jpg"}))
+                .placeId("1641ac95-da05-4e76-aecd-8b524d5e408f")
+                .userId("73d193ed-5158-499b-9bd3-d5763e57bb42")
+                .build());
+
+        reviews.add(review);
+
+        for (Review r : reviews) {
+            assertThat(r.getUserId()).isEqualTo("73d193ed-5158-499b-9bd3-d5763e57bb42");
         }
     }
     @Test
     void getUserIdsByPlaceIdTest() {
-        List<String> userIds = reviewService.getUserIdsByPlaceId("b0e96d94-1c85-416d-bc35-8b3017dd5a65");
+        List<Review> reviews = new LinkedList<>();
+        Review review = reviewRepository.save(Review.builder()
+                .content("getUserIdsByPlaceIdTest")
+                .attachedPhotoIds(List.of(new String[]{"image1.jpg",
+                        "image2.png", "image3.jpg"}))
+                .placeId("405ac2ad-b54e-4207-8f58-1ccba3848ead")
+                .userId("73d193ed-5158-499b-9bd3-d5763e57bb42")
+                .build());
+
+        reviews.add(review);
+
+        List<String> userIds = reviewService.getUserIdsByPlaceId("405ac2ad-b54e-4207-8f58-1ccba3848ead");
+
         for (String userId : userIds) {
             for (Review r : reviewService.getReviewsByUserId(userId)){
-                assertThat(r.getPlaceId()).isEqualTo("b0e96d94-1c85-416d-bc35-8b3017dd5a65");
+                assertThat(r.getPlaceId()).isEqualTo("405ac2ad-b54e-4207-8f58-1ccba3848ead");
             }
         }
     }
 
     @Test
     void checkPhotoTest() {
-        Review review = reviewRepository.findById("5e5ac8c0-9e5e-484a-8cb3-d84230d5d93f").get();
+        Review review = Review.builder()
+                .content("checkPhotoTest")
+                .attachedPhotoIds(List.of(new String[]{"image1.jpg",
+                        "image2.png", "image3.jpg"}))
+                .placeId("65553057-d3de-4c34-9b19-6ffff381c47f")
+                .userId("b0e96d95-1c85-416d-bc35-8b3017dd5a65")
+                .build();
+
         if(review.getAttachedPhotoIds().isEmpty()){
-            assertThat(reviewService.checkPhoto(review).getKey()).isEqualTo(0);
+            assertThat(reviewService.checkPhoto(review)).isFalse();
         }else{
-            assertThat(reviewService.checkPhoto(review).getKey()).isEqualTo(1);
+            assertThat(reviewService.checkPhoto(review)).isTrue();
         }
     }
 
     @Test
     void checkContentTest() {
-        Review review = reviewRepository.findById("5e5ac8c0-9e5e-484a-8cb3-d84230d5d93f").get();
+        Review review = reviewRepository.save(Review.builder()
+                .content("checkContentTest")
+                .attachedPhotoIds(List.of(new String[]{"image1.jpg",
+                        "image2.png", "image3.jpg"}))
+                .placeId("6044507f-d303-4d41-bec8-b4ebce6d8da3")
+                .userId("42ab933f-2f37-4131-a784-8e4f889418c9")
+                .build());
+
         if(review.getContent().isEmpty()){
-            assertThat(reviewService.checkContent(review).getKey()).isEqualTo(0);
+            assertThat(reviewService.checkContent(review)).isFalse();
         }else{
-            assertThat(reviewService.checkContent(review).getKey()).isEqualTo(1);
+            assertThat(reviewService.checkContent(review)).isTrue();
         }
     }
     @Test
     void visitedCheckTest() {
-        Review review = Review.builder()
+        Review review = reviewRepository.save(Review.builder()
                 .content("사장님이 친절하고 음식이 맛있어요.")
                 .attachedPhotoIds(List.of(new String[]{"image1.jpg",
                         "image2.png", "image3.jpg"}))
-                .placeId("5044507f-d303-4d41-bec8-b4ebce6d8da3")
+                .placeId("4443a517-2147-4f42-9ec7-96281035d6af")
                 .userId("42ab933f-2f37-4131-a784-8e4f889418c9")
-                .build();
+                .build());
 
         boolean ck = reviewService.visitedCheck(review);
         if(reviewService.getUserIdsByPlaceId(review.getPlaceId()).isEmpty()){
@@ -85,22 +133,35 @@ class ReviewServiceTest {
 
     @Test
     void checkPlaceTest() {
-        Review review = reviewRepository.findById("5e5ac8c0-9e5e-484a-8cb3-d84230d5d93f").get();
+        Review review = Review.builder()
+                .content("checkPlaceTest")
+                .attachedPhotoIds(List.of(new String[]{"image1.jpg",
+                        "image2.png", "image3.jpg"}))
+                .placeId("69da8289-6d34-45f5-9525-2b7539bf4eb0")
+                .userId("b0e96d95-1c85-416d-bc35-8b3017dd5a65")
+                .build();
+
         if(reviewService.visitedCheck(review)){
-            assertThat(reviewService.checkPlace(review).getKey()).isEqualTo(0);
+            assertThat(reviewService.checkPlace(review)).isFalse();
         }else{
-            assertThat(reviewService.checkPlace(review).getKey()).isEqualTo(1);
+            assertThat(reviewService.checkPlace(review)).isTrue();
         }
     }
 
     @Test
     void checkAmountTest() {
-        Review review = reviewRepository.findById("5e5ac8c0-9e5e-484a-8cb3-d84230d5d93f").get();
         int amount = 0;
+        Review review = Review.builder()
+                .content("checkAmountTest")
+                .attachedPhotoIds(List.of(new String[]{"image1.jpg",
+                        "image2.png", "image3.jpg"}))
+                .placeId("64772458-8504-5ac8-9315-37b2c720c7e9")
+                .userId("b0e96d95-1c85-416d-bc35-8b3017dd5a65")
+                .build();
 
-        if (reviewService.checkPhoto(review).getKey() > 0) amount++;
-        if (reviewService.checkContent(review).getKey() > 0) amount++;
-        if (reviewService.checkPlace(review).getKey() > 0) amount++;
+        if (reviewService.checkPhoto(review)) amount++;
+        if (reviewService.checkContent(review)) amount++;
+        if(amount > 0){ if (reviewService.checkPlace(review)) amount++;}
 
         assertThat(reviewService.checkAmount(review).getKey()).isEqualTo(amount);
     }
@@ -113,11 +174,11 @@ class ReviewServiceTest {
     @Test
     void insertReviewTest() {
         ReviewDto reviewDto = reviewService.insertReview(Review.builder()
-                .content("0701 리뷰 추가 테스트")
+                .content("insertReviewTest")
                 .attachedPhotoIds(List.of(new String[]{"image1.jpg",
                         "image2.png", "image3.jpg"}))
-                .placeId("69da8289-6d24-45f5-9525-2b7539bf4eb0")
-                .userId("a0e96d95-1c85-416d-bc35-8b3017dd5a65")
+                .placeId("0c1a7704-ceb3-4fef-90c5-2277dffc6cc2")
+                .userId("b0e96d95-1c85-416d-bc35-8b3017dd5a65")
                 .build());
         assertNotNull(reviewDto.getReviewId());
     }
