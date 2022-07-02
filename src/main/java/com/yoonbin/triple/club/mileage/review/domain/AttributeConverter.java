efef75dc0343057ac80b7a1743ad838535d8c73e
Ayoon-b <wnr0303@gmail.com>
2022-07-02 21:21:12 +0900

    chore: 쓰이지 않는 클래스 제거

--- src/main/java/com/yoonbin/triple/club/mileage/review/domain/AttributeConverter.java
+++
@@ -1,8 +0,0 @@
-package com.yoonbin.triple.club.mileage.review.domain;
-
-public interface AttributeConverter <X, Y> {
-
-    Y convertToDatabaseColumn(X var1);
-
-    X convertToEntityAttribute(Y var1);
-}
