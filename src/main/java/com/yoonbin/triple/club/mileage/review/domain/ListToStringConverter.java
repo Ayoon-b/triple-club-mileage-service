83aa6d1e48ef54a7ca148578f0a37d632aadb0b9
Ayoon-b <wnr0303@gmail.com>
2022-07-02 23:40:32 +0900

    fix: 문자열이 비어있는 경우 비어있는 문자열이 포함된 리스트로 변환되는 문제 해결

--- src/main/java/com/yoonbin/triple/club/mileage/review/domain/ListToStringConverter.java
+++ src/main/java/com/yoonbin/triple/club/mileage/review/domain/ListToStringConverter.java
@@ -4,6 +4,7 @@ import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Converter
 public class ListToStringConverter implements AttributeConverter<List, String> {
@@ -13,8 +14,8 @@ public class ListToStringConverter implements AttributeConverter<List, String> {
     }
 
     @Override
-    public List<String> convertToEntityAttribute(String s) {
-        return Arrays.asList(s.split(","));
+    public List<String> convertToEntityAttribute(String str) {
+        return Arrays.stream(str.split(",")).filter(s -> !s.isEmpty()).collect(Collectors.toList());
     }
 }
 
